852ed9bd73091f039db0cf92d5a9288117f6c73f
ilewis01 <mrredd32@gmail.com>
2017-09-21 02:23:16 -0400

    Finished JS for new account

--- LewisBanking/bank/functions.py
+++ LewisBanking/bank/functions.py
@@ -4,8 +4,39 @@ from datetime import date
 import random
 import json
 import json as simplejson
-from bank.models import profile
+
+from bank.models import profile, Account, Loan
 
 
-def getAccount(request):
-	no = None
+def fetchSecurityQuestions1():
+	questions = []
+
+	for i in range(5):
+		q = {}
+		q['index'] = i
+		questions.append(q)
+
+	questions[0]['question'] = "What is your mother's maiden name?"
+	questions[1]['question'] = "What high school did you attend?"
+	questions[2]['question'] = "What is you maternal grandmother's name?"
+	questions[3]['question'] = "What is you paternal grandmother's name?"
+	questions[4]['question'] = "What is you maternal grandfather's name?"
+
+
+	return questions
+
+def fetchSecurityQuestions2():
+	questions = []
+
+	for i in range(5):
+		q = {}
+		q['index'] = i
+		questions.append(q)
+
+	questions[0]['question'] = "What is you paternal grandfather's name?"
+	questions[1]['question'] = "What is the name of the street where you grew up?"
+	questions[2]['question'] = "What is your favorite food?"
+	questions[3]['question'] = "What is your childhood best friend's name?"
+	questions[4]['question'] = "What is the name of your elementary school?"
+
+	return questions
--- LewisBanking/static/static_dirs/js/custom.js
+++ LewisBanking/static/static_dirs/js/custom.js
@@ -32,4 +32,214 @@ function visibility(z_index, mode)
 	{
 		div.className = "fadeOut";
 	}
+
 }
+
+function load_popWin(z_div, index, target, width, height, message_type) 
+{
+	target.style.width = width;
+	target.style.height = height;
+
+	z_div.style.zIndex = index;
+	z_div.className = "fadeIn";
+
+	if (message_type === 'terms')
+	{
+		load_terms_html();
+	}
+
+	else if (message_type === 'errors')
+	{
+		load_error_html();
+	}
+}
+
+function load_terms_html()
+{
+	grab('fname').value = "Loading Terms";
+}
+
+function load_error_html()
+{
+	grab('fname').value = "Loading Errors";
+}
+
+function entry_error(target, mode, max_length)
+{
+	var value = String(target.value);
+
+	if (mode === "text")
+	{
+		target.value = text_only(value);
+	}
+
+	else if (mode === "number")
+	{
+		target.value = number_only(value, max_length);
+	}
+
+	else if (mode === "money")
+	{
+		target.value = money_only(value, max_length);
+	}
+}
+
+function number_only(value, max_length)
+{
+	var edited = "";
+	var v = "";
+
+	for (var i = 0; i < max_length; i++)
+	{
+		v = value.charAt(i);
+
+		if (v==="1" || v==="2" || v==="3" || v==="4" || v==="5" || v==="6" || v==="7" || v==="8" || v=="9" || v==="0")
+		{
+			edited += v;
+		}
+	}
+
+	return edited;
+}
+
+function text_only(value)
+{
+	var edited = "";
+	var v = "";
+
+	for (var i = 0; i < value.length; i++)
+	{
+		v = value.charAt(i);
+
+		if (v!=="0" && v!=="1" && v!=="2" && v!=="3" && v!=="4" && v!=="5" && v!=="6" && v!=="7" && v!=="8" && v!=="9")
+		{
+			edited += v;
+		}
+	}
+
+	return edited;
+}
+
+function money_only(value, max_length)
+{
+	var edited = "";
+	var v = "";
+
+	for (var i = 0; i < max_length; i++)
+	{
+		v = value.charAt(i);
+
+		if (v==="1" || v==="2" || v==="3" || v==="4" || v==="5" || v==="6" || v==="7" || v==="8" || v=="9" || v==="0" || v===".")
+		{
+			edited += v;
+		}
+	}
+
+	return edited;
+}
+
+function switchAccountSubmit()
+{
+	var data = [];
+	var button = grab('creBtn');
+	var proceed = true;
+
+	data.push(String(grab('fname').value));
+	data.push(String(grab('lname').value));
+	data.push(String(grab('email').value));
+	data.push(String(grab('email2').value));
+	data.push(String(grab('password1').value));
+	data.push(String(grab('password2').value));	
+	data.push(String(grab('areaCode').value));
+	data.push(String(grab('phone2').value));
+	data.push(String(grab('postfix').value));
+	data.push(String(grab('deposit').value));
+	data.push(String(grab('answer1').value));
+	data.push(String(grab('answer2').value));
+
+	for (var i = 0; i < data.length; i++)
+	{
+		if (data[i].length === 0)
+		{
+			proceed = false;
+			break;
+		}
+	}
+
+	if (proceed === true)
+	{
+		button.setAttribute('type', 'button');
+		button.setAttribute('onClick', "javascript: field_validation_account();")
+	}
+
+	else
+	{
+		button.setAttribute('type', 'submit');
+		button.removeAttribute('onClick');
+	}
+}
+
+function field_validation_account()
+{
+	var data = [];
+	var proceed = true;
+	var email = grab('email');
+	var emai2 = grab('email2');
+	var pass1 = grab('password1');
+	var pass2 = grab('password2');
+	var depos = grab('deposit');
+	
+	if (String(pass1.value) !== String(pass2.value))
+	{
+		proceed = false;
+		data.push("The passwords do not match");
+	}
+
+	if (String(email.value) !== String(emai2.value))
+	{
+		proceed = false;
+		data.push("The email addresses do not match");
+	}
+
+	if (Number(depos.value) < 1)
+	{
+		proceed = false;
+		data.push("The initial deposit amount must be at least $1.00")
+	}
+
+	if (proceed === true)
+	{
+		var button = grab('creBtn');
+		button.setAttribute('type', 'submit');
+		button.removeAttribute('onClick');
+		button.click();
+	}
+	else
+	{
+		grab('messageContent').innerHTML = get_error_html(data);
+		load_popWin(grab("z1"), 1, grab("messageWindow"), "400px", "200px", "errors");
+	}
+}
+
+function get_error_html(error_list)
+{
+	var html = "<ul>";
+
+	for (var i = 0; i < error_list.length; i++)
+	{
+		html += "<li>" + error_list[i] + "</li>";
+	}
+
+	html += "</ul>";
+	return html;
+}
+
+
+
+
+
+
+
+
+
+
